e41beefafb7faa3802c1d04848d05dadd5193295
Dayane Sousa <dayanesousa1516@gmail.com>
2026-06-02 10:21:08 -0300

    Create python_calculadora.py
    creat file interação com úsuario

---
+++ python_calculadora.py
@@ -0,0 +1,17 @@
+#Vendas de madeira
+vendas = int(input("Informe o valor vendido: "))
+print (f"Foi vendido {vendas} reais.")
+
+#Custos com madeira de 40%
+custo = vendas * 0.4
+print (f"O custo para produção foi de {custo} reais.")
+
+#Bônus para funcionarios
+vendas  >= 1000
+bonus = 0.10 *1000
+print (f"O bonus foi de {bonus} reais.")
+
+#Valor restante de lucro
+lucro = vendas - custo - bonus
+print (f"O lucro final foi de {lucro} reais.") 
+
